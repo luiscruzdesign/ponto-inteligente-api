d350d79efb477a4477770603ec1c71b740e758a8
Luis Eduardo setin de carvalho cruz <icruz@Mac-mini-de-Administrador.local>
2019-05-15 15:12:01 -0300

    Controller cadastro PJ

--- src/main/java/com/example/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/example/pontointeligente/api/PontoInteligenteApplication.java
@@ -2,8 +2,9 @@ package com.example.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
